3222c5f37d6e1156429a850fc0f26e63582c70c7
Ashanti <AshantiMutinta@gmail.com>
2018-04-03 19:28:31 -0400

    added new token file

---
+++ src/token_analyzer/token.rs
@@ -0,0 +1,70 @@
+use std::cmp::Ord;
+use std::cmp::Ordering;
+
+#[derive(Eq)]
+pub struct TokenMatch<'a>
+{
+    pub literal : String,
+    pub begin_segmet : usize,
+    pub end_segment : usize,
+    pub rule : &'a TokenRules<'a>
+}
+
+#[derive(PartialEq,Eq,Debug)]
+pub enum TokenError
+{
+    EmptyLineOfCode,
+    MatchingError,
+    NoMatch
+}
+
+#[derive(PartialEq,Eq,Debug)]
+pub enum TokenType
+{
+    Alphanumeric,
+    Literal,
+    Number,
+    Keyword,
+    Space,
+    SpecialCharacter,
+    EndingTerminator
+}
+
+#[derive(PartialEq,Eq,Debug)]
+pub struct Token<'a>
+{
+    pub content : String,
+    pub token_type : &'a TokenType
+}
+
+#[derive(PartialEq,Eq)]
+pub struct TokenRules<'a>
+{
+    pub token_type : &'a TokenType,
+    pub regex_rule: String,
+}
+
+
+impl<'a> Ord for TokenMatch<'a>
+{
+    fn cmp(&self,other:&TokenMatch) -> Ordering
+    {
+        self.begin_segmet.cmp(&other.begin_segmet)
+    }
+}
+
+
+impl<'a> PartialOrd for TokenMatch<'a> {
+    fn partial_cmp(&self, other: &TokenMatch) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> PartialEq for TokenMatch<'a> {
+    fn eq(&self, other: &TokenMatch) -> bool 
+    {
+        (self.literal == other.literal) && 
+        (self.begin_segmet== other.begin_segmet) &&
+        (self.end_segment == other.end_segment)
+    }
+}
